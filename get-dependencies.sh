6a21ee85fc4111b2cd3e11bda28b294d23c96024
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-14 10:07:27 -0400

    add pipewire and build git version because stable is too old

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,13 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm pipewire-audio pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
-get-aur-package sview
+get-aur-package sview-git
 
 # If the application needs to be manually built that has to be done down here
