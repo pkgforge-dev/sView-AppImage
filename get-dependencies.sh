8fbcb2cc92200833c401c1f54797b35526ae26b0
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-17 11:24:48 -0400

    `ttf-dejavu`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pipewire-audio pipewire-jack
+pacman -Syu --noconfirm pipewire-audio pipewire-jack ttf-dejavu
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
