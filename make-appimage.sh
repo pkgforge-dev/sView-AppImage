bac79da3c8a483d3f564b39d8e28aa9f8c33c739
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-13 16:35:26 -0400

    add test

--- make-appimage.sh
+++ make-appimage.sh
@@ -23,3 +23,7 @@ quick-sharun \
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --test ./dist/*.AppImage
