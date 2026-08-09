44e6f84f7f7dda79e112624a06ce098bea50b28f
Samueru-sama <xdglawyer@outlook.com>
2026-08-09 11:33:19 -0400

    build manually instead of using the AUR

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,15 +6,34 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm pipewire-audio pipewire-jack ttf-dejavu
+pacman -Syu --noconfirm \
+	cmake          \
+	libconfig      \
+	libxext        \
+	libxpm         \
+	openal         \
+	pipewire-audio \
+	pipewire-jack  \
+	ttf-dejavu
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
-# According to upstream release notes gtk2 is no longer needed
-# also the AUR packager forgot to include cmake in the dependencies
 make-aur-package zenity-rs-bin
-PRE_BUILD_CMDS="sed -i -e 's|gtk2|cmake|g' ./PKGBUILD" make-aur-package sview-git
 
 # If the application needs to be manually built that has to be done down here
+echo "Building sView..."
+echo "---------------------------------------------------------------"
+git clone https://github.com/gkv311/sview ./sview
+cd ./sview
+
+git fetch --tags origin
+TAG=$(git tag --sort=-v:refname | grep -vi 'preview\|alpha\|beta' | head -1)
+git checkout "$TAG"
+
+cmake -S ./ -B build -D CMAKE_INSTALL_PREFIX=/usr -D USE_OPENVR=OFF
+cmake --build build
+cmake --install build
+
+echo "$TAG" > ~/version
