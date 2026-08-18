13be3cdc19093ee1e97f8fa9473fbe06f0084c3e
Link4Electronics <link4electronics@gmail.com>
2026-08-18 20:20:04 -0300

    update template and hook (#10)

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,7 +5,7 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/gkv311/sview/refs/heads/master/share/icons/hicolor/256x256/apps/sview.png
 export DESKTOP=https://raw.githubusercontent.com/gkv311/sview/refs/heads/master/share/applications/sViewMP.desktop
